170e252a56e7e3644a09211357387e1aa5155fec
serejja <serejja@gmail.com>
2016-03-14 14:18:21 +0200

    Build fixes

--- task.go
+++ task.go
@@ -1,5 +1,7 @@
 package utils
 
+import mesos "github.com/mesos/mesos-go/mesosproto"
+
 type TaskConfig map[string]string
 
 func (tc TaskConfig) GetString(key string) string {
@@ -9,4 +11,15 @@ func (tc TaskConfig) GetString(key string) string {
 	}
 
 	return value
+}
+
+func TaskMatches(data *TaskData, offer *mesos.Offer) string {
+	if data.Cpu > GetScalarResources(offer, "cpus") {
+		return "no cpus"
+	}
+
+	if data.Mem > GetScalarResources(offer, "mem") {
+		return "no mem"
+	}
+	return ""
 }
